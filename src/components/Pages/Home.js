aa153847f1d03f2447070abd18eb8d202c60b52f
Michael Taggart <mtaggarthawk@gmail.com>
2021-05-11 17:14:53 -0600

    working navigation bar

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -4,7 +4,10 @@ export default class Home extends Component {
  render() {
   return (
    <div>
-
+    <div>
+     <h1 className="name">Michael J.H. Taggart</h1>
+     <h3 className="sub-name">Full Stack Web Developer</h3>
+    </div>
    </div>
   )
  }
